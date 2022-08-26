bf7aa2fcf2ee6099fa6b9c5532c7e6e94c92f10f
im-shubhamsharma <im_shubhamsharma@outlook.com>
2022-08-27 01:34:17 +0530

    added animation to home page

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -5,9 +5,15 @@ function Cover(props) {
     <div className="cover">
       <div id="name"> &lt; SHUBHAM SHARMA /&gt; </div>
 
-      <div id="dev"> &lt;&#183;&#183;&#183;&#183;WEB DEVELOPER </div>
+      <div id="dev">
+        {" "}
+        &lt;&#183;&#183;&#183;&#183;WEB
+        DEVELOPER&lt;&#183;&#183;&#183;&#183;DESIGNER
+      </div>
 
-      <div id="under-construction">----SITE UNDER CONSTRUCTION ---- </div>
+      <div id="under-construction">
+        ---------SITE UNDER CONSTRUCTION--------SITE UNDER CONSTRUCTION--------SITE UNDER CONSTRUCTION----
+      </div>
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,25 @@
-import { DeveloperBoard } from '@mui/icons-material';
-import React from 'react';
+import { DeveloperBoard } from "@mui/icons-material";
+import React from "react";
 
 function Home(props) {
-    return (
-      <div className="home">
-        <div className="image"></div>
-        <h1 id="hey">Hey There, I'm</h1>
-        <h1 id="name">Shubham Sharma!</h1>
-        <h1 id="work">I'm a Frontend Developer</h1>
-       
+  return (
+    <div className="home">
+      <div className="image"></div>
+      <h1 id="hey">Hey There, I'm</h1>
+      <h1 id="name">Shubham Sharma!</h1>
+      <div className="dynamic-text">
+        <div id="static">I'm a</div>
+        <div id="dynamic">
+          <div id="word1">
+            <span>Frontend&nbsp;Developer</span>
+          </div>
+          <div id="word2">Designer</div>
+          <div id="word3">Optimist</div>
+          <div id="word4">Seeker</div>
+        </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Home;
